e5eab3c440b36f4203395812485e055f96c37814
healer_kx <healer.kx.yu@gmail.com>
2014-09-28 21:41:14 +0800

    GIS point of 2 stations

--- www/view/media/widgets/MapWidget.js
+++ www/view/media/widgets/MapWidget.js
@@ -13,22 +13,30 @@ $class("BaiduMap", [kx.Widget, kx.ActionMixin, kx.EventMixin],
     showMap: function() {
 
         // 大气站
+        var cy = 22.051178333;
+        var cx = 113.18576333;
+        var gpsCenter = new BMap.Point(cx, cy);
+
+        // 大气站
         var y1 = 22.02178333;
         var x1 = 113.21576333;
         var gpsPoint1 = new BMap.Point(x1, y1);
 
         // 水库
-        var y2 = 22.02178333;
+        var y2 = 22.0288333;
         var x2 = 113.21586533;
         var gpsPoint2 = new BMap.Point(x2, y2);
 
         //地图初始化
         var map = new BMap.Map("allmap");
-        map.centerAndZoom(gpsPoint2, 14);
+        map.centerAndZoom(gpsCenter, 14);
         map.addControl(new BMap.NavigationControl());
 
+        var this_ = this;
         this.addStation(map, gpsPoint1, "大气辐射环境自动监测站");
-        this.addStation(map, gpsPoint2, "竹银水库水质监测自动站");
+        setTimeout(function(){
+            this_.addStation(map, gpsPoint2, "竹银水库水质监测自动站");
+        }, 500);
     },
 
     addStation: function(map, gpsPoint, text) {
